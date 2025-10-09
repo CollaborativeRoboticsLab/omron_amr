1c2ade9ba81e06d231d6f2ccc64a7c78e889bd1b
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2025-10-09 08:36:06 +0000

    added multi threaded executors

--- amr_core/src/core_node.cpp
+++ amr_core/src/core_node.cpp
@@ -6,7 +6,12 @@ int main(int argc, char** argv)
   rclcpp::init(argc, argv);
   auto node = std::make_shared<CoreNode>();
   node->initialize();
-  rclcpp::spin(node);
+
+  // Use MultiThreadedExecutor for concurrent callbacks
+  rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(node);
+  executor.spin();
+
   rclcpp::shutdown();
   return 0;
 }
