036d45e5181f5c0adf6d03db14ea4c08e391ecae
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2025-10-15 09:05:56 +0000

    optimized launch files

--- amr_ros/launch/amr_core.launch.py
+++ amr_ros/launch/amr_core.launch.py
@@ -1,53 +1,56 @@
+import sys
+import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
 from launch_ros.actions import Node
+from launch.conditions import UnlessCondition
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
-import os
-
 
 def load_file(package_name, file_path):
     package_path = get_package_share_directory(package_name)
     absolute_file_path = os.path.join(package_path, file_path)
+
     try:
         with open(absolute_file_path, 'r') as file:
             return file.read()
-    except EnvironmentError:
+    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
         return None
 
 
 def generate_launch_description():
-    robot_description = LaunchConfiguration('robot_description')
-    
-    # Launch arg: robot_description as raw XML string
-    declare_robot_description = DeclareLaunchArgument(
-        'robot_description',
-        default_value=load_file('amr_description', 'urdf/LD250.urdf') or '',
-        description='URDF XML for robot_state_publisher'
+    robot_description_override = LaunchConfiguration('robot_description_override')
+
+    declare_robot_description_override = DeclareLaunchArgument(
+        'robot_description_override',
+        default_value='false',
+        description='URDF/Xacro file path inside the package'
     )
 
-    core_parms = os.path.join(get_package_share_directory('amr_ros'), 'config', 'parameters.yaml')
-
-    core = Node(
-        package='amr_core',
-        executable='amr_core',
-        name='amr_core',
-        output='screen',
-        parameters=[core_parms],
-    )
+    robot_description = {'robot_description': load_file('amr_description', 'urdf/LD250.urdf')}
 
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='log',
-        parameters=[{'robot_description': robot_description}],
+        parameters=[robot_description],
+        condition=UnlessCondition(robot_description_override)
     )
 
-    ld = LaunchDescription()
-    ld.add_action(declare_robot_description)
-    ld.add_action(core)
-    ld.add_action(robot_state_publisher)
+    core = Node(
+        package='amr_core',
+        executable='amr_core',
+        name='amr_core',
+        output='screen',
+        parameters=[],
+    )
 
-    return ld
+    return LaunchDescription([
+        declare_robot_description_override,
+        robot_state_publisher,
+        core,
+    ])
 
