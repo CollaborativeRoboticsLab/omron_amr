798498c6f4240f863fefa63a014b5827d610b852
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2025-10-15 05:12:21 +0000

    minor changes

--- amr_ros/launch/amr_core.launch.py
+++ amr_ros/launch/amr_core.launch.py
@@ -1,26 +1,30 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
+
 def load_file(package_name, file_path):
     package_path = get_package_share_directory(package_name)
     absolute_file_path = os.path.join(package_path, file_path)
-
     try:
         with open(absolute_file_path, 'r') as file:
             return file.read()
-    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
+    except EnvironmentError:
         return None
+
 
 def generate_launch_description():
     robot_description = LaunchConfiguration('robot_description')
-
+    
+    # Launch arg: robot_description as raw XML string
     declare_robot_description = DeclareLaunchArgument(
-            'robot_description', 
-            default={'robot_description' : load_file('amr_description', 'urdf/LD250.urdf')},
-            description='Target robot description file'
-        )
+        'robot_description',
+        default_value=load_file('amr_description', 'urdf/LD250.urdf') or '',
+        description='URDF XML for robot_state_publisher'
+    )
 
     core_parms = os.path.join(get_package_share_directory('amr_ros'), 'config', 'parameters.yaml')
 
@@ -32,20 +36,18 @@ def generate_launch_description():
         parameters=[core_parms],
     )
 
-    # Publish Robot
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='log',
-        parameters=[robot_description],
+        parameters=[{'robot_description': robot_description}],
     )
 
     ld = LaunchDescription()
-
-    ld.add_action(core)
     ld.add_action(declare_robot_description)
+    ld.add_action(core)
     ld.add_action(robot_state_publisher)
-    
+
     return ld
 
